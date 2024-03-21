2e11ca93ea905bfe3db457aca0ae1788305a7fdc
loet <loet@OA-S141-21>
2024-03-21 11:01:23 -0400

    dropdown and slider added but incomplete

---
+++ Assets/Week 10/Scripts/Clock.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    public Slider slider;
+    float time;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime * speed;
+        time = time % 60;
+
+    }
+}
