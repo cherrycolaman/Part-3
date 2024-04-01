3d2cfc5172aefa01fe54fc28872f5e89a0b222a4
DESKTOP-5A2LTEH\GamerTech <loet@sheridancollege.ca>
2024-03-31 20:43:04 -0400

    added UI stuff and description text script

---
+++ Assets/Assignment/Scripts/Description.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Description : MonoBehaviour
+{
+    public TextMeshProUGUI descriptionText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
